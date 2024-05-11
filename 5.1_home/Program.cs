e0147f4d14bd14528050d7f4d61533ea69cb5c62
Artemonchick <nastspin0@gmail.com>
2024-05-11 13:29:50 +0500

    Добавьте файлы проекта.

---
+++ 5.1_home/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._2
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Метод разделения входной фразы на слова
+        /// </summary>
+        /// <param name="text">Строка, которую необходимо разделить</param>
+        /// <returns>Массив слов из предложения</returns>
+        public static string[] Reverse(string text)
+        {
+            char[] separators = new char[] { ' ', '.', ',', '-', '!', '?', '"', ':' };
+
+            string[] subs = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            return subs;
+        }
+
+        /// <summary>
+        /// Метод изменения порядка слов
+        /// </summary>
+        /// <param name="text">Предложение, разобранное по словам</param>
+        /// <returns>Слова в обратном порядке</returns>
+        static string ReversWords(string text)
+        {
+            string[] FinalText = Reverse(text);
+            Array.Reverse(FinalText);
+
+            string NewString = "";
+            for (int i = 0; i <= FinalText.Length - 1; i++)
+            {
+                NewString += FinalText[i] + " ";
+            }
+
+            return NewString;
+        }
+
+        /// <summary>
+        /// Метод для вывода каждого слова в отдельной строке
+        /// </summary>
+        /// <param name="FinalText">Разделённая строка</param>
+        static void Print(string ReadyText)
+        {
+            Console.Write(ReversWords(ReadyText));
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите предложение: ");
+            string InputPhrase = Console.ReadLine();
+
+            ReversWords(InputPhrase);
+            Print(InputPhrase);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 5_home/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_1
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Метод разделения строки на слова
+        /// </summary>
+        /// <param name="text">Строка, которую необходимо разделить</param>
+        /// <returns></returns>
+        static string[] SplitText(string text)
+        {
+            char[] separators = new char[] { ' ', '.', ',', '-', '!', '?', '"', ':' };
+
+            string[] subs = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            return subs;
+        }
+
+        /// <summary>
+        /// Метод для вывода каждого слова в отдельной строке
+        /// </summary>
+        /// <param name="FinalText">Разделённая строка</param>
+        static void Print(string subs)
+        {
+
+            foreach (var sub in SplitText(subs))
+            {
+                Console.WriteLine($"{sub}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите предложение: ");
+            string sentence = Console.ReadLine();
+
+            SplitText(sentence);
+            Print(sentence);
+
+            Console.ReadKey();
+        }
+    }
+}
